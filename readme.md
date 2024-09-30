a92e7f5c0b0a299c3108aa4cd1405de9c5f22eab
VictorLins11 <victorasglins@gmail.com>
2024-09-30 00:54:16 -0300

    ✅Aplicativo Clima Web

---
+++ readme.md
@@ -0,0 +1,45 @@
+# Aplicativo Clima-Web
+
+Este é o Aplicativo Clima-Web, uma página de status do tempo desenvolvido utilizando a API OpenWeatherMap. Neste aplicativo, criei uma página simples e responsiva que permite aos usuários buscar a previsão do tempo de uma cidade e exibe informações como temperatura, velocidade do vento e condiçoes do céu.
+
+https://github.com/VictorLins11/Aplicativo-Clima/images/Captura_de_tela.png
+
+## Descrição do Aplicativo
+
+O Aplicativo Clima-Web é uma aplicação web que utiliza a API OpenWeatherMap para fornecer informações status do tempo em tempo real para uma cidade específica. Os usuários podem inserir o nome da cidade na cixa de pesquisa e receberão informações atualizadas sobre a clima naquela localidade.
+
+## Funcionalidades Principais
+
+- Busca de previsão do tempo com base no nome da cidade.
+- Exibição de informações como temperatura, velocidade do vento e condições do céu.
+- Design responsivo que se adapta a diferentes tamanhos de tela.
+
+## Tecnologias Utilizadas
+
+- HTML5
+- CSS3
+- JavaScript
+- API OpenWeatherMap
+
+## Instalação
+
+Não é necessário instalação para esse aplicativo. Basta abrir o arquivo `index.html` em um navegador da web para acessar a página de previsão do tempo.
+
+```bash
+# clone o repositório (caso esteja utilizando um sistema de controle de versão)
+git clone https://github.com/VictorLins11/Aplicativo-Clima.git
+
+# Navegue até o diretório do aplicativo
+cd nome-do-repositório
+```
+
+## Como Usar
+
+1. Abra o arquivo `index.html` em seu navegador da web.
+2. Na caixa de pesquisa, digite o nome da cidade para a qual deseja obter a previsão do tempo.
+3. Pressione o botão de pesquisa ou pressione "Enter" no teclado.
+4. As informações de previsão do tempo para a cidade especificada serão exibidas na tela, incluindo temperatura, velocidade do vento e condições do céu.
+
+## Contribuição
+
+Este é um projeto pessoal. Portanto fico feliz em ouvir seus comentários e sugestões!
---
+++ script.js
@@ -0,0 +1,118 @@
+let weather = {
+  apiKey: "aba6ff9d6de967d5eac6fd79114693cc",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("Nenhum clima encontrado.");
+          throw new Error("Nenhum clima encontrado.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Tempo em " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Umidade: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Velocidade do vento: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+let geocode = {
+  reverseGeocode: function (latitude, longitude) {
+    var apikey = "90a096f90b3e4715b6f2e536d934c5af";
+
+    var api_url = "https://api.opencagedata.com/geocode/v1/json";
+
+    var request_url =
+      api_url +
+      "?" +
+      "key=" +
+      apikey +
+      "&q=" +
+      encodeURIComponent(latitude + "," + longitude) +
+      "&pretty=1" +
+      "&no_annotations=1";
+
+    var request = new XMLHttpRequest();
+    request.open("GET", request_url, true);
+
+    request.onload = function () {
+
+      if (request.status == 200) {
+        var data = JSON.parse(request.responseText);
+        weather.fetchWeather(data.results[0].components.city);
+        console.log(data.results[0].components.city)
+      } else if (request.status <= 500) {
+
+        console.log("Incapaz de geocodificar! Código de resposta: " + request.status);
+        var data = JSON.parse(request.responseText);
+        console.log("Mensagem de erro: " + data.status.message);
+      } else {
+        console.log("Erro de servidor.");
+      }
+    };
+
+    request.onerror = function () {
+      console.log("Não consigo conectar ao servidor.");
+    };
+
+    request.send(); 
+  },
+  getLocation: function() {
+    function success (data) {
+      geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+    }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, console.error);
+    }
+    else {
+      weather.fetchWeather("New York");
+    }
+  }
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("New York");
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+geocode.getLocation();
